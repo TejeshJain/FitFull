4d496fdb3ba14c8671eef3679dec7ab844516627
Tejesh Jain <tejeshjain2521@gmail.com>
2021-11-25 20:12:03 +0530

    Second Commit

--- Resources/Java Script/script.js
+++ Resources/Java Script/script.js
@@ -82,3 +82,15 @@ $(document).ready(function() {
         }
     });
 });
+
+const readMoreBtn = document.querySelector(".read-more-btn");
+const text = document.querySelector(".text");
+
+readMoreBtn.addEventListener("click", (e) => {
+  text.classList.toggle("show-more");
+  if (readMoreBtn.innerText === "Read More") {
+    readMoreBtn.innerText = "Read Less";
+  } else {
+    readMoreBtn.innerText = "Read More";
+  }
+});
